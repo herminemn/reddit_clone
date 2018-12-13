f430a5715245d04434b219672b7645c5099266e2
herminemn <hermine.mn@gmail.com>
2018-12-13 23:18:37 +0400

    Created Sign Up Page

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'This username already exists'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                login(request, user)
+                return render(request, 'accounts/signup.html')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords don\'t match'})
+    else:
+        return render(request, 'accounts/signup.html')
